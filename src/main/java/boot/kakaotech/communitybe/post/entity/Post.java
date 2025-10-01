4334a757cf203eabb6692c1c373f98023432d79d
JaeSeoHan <hanahyun1@gmail.com>
2025-10-01 16:02:58 +0900

    feat: Post 엔티티 작성[한재서]
    - summary:
      Post 엔티티 작성
    
    - changes:
      1. erd에 설계한 post 테이블에 맞게 엔티티 작성

--- src/main/java/boot/kakaotech/communitybe/post/entity/Post.java
+++ src/main/java/boot/kakaotech/communitybe/post/entity/Post.java
@@ -1,4 +1,41 @@
 package boot.kakaotech.communitybe.post.entity;
 
+import boot.kakaotech.communitybe.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "post")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    @Column(length = 30, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private int viewCount;
+
+    @Column(columnDefinition = "TIMESTAMP", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime deletedAt;
+
 }
