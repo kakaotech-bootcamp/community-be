d211bcc018baba99c77d0d6b4cfa946a1c39ee66
JaeSeoHan <hanahyun1@gmail.com>
2025-10-01 14:32:41 +0900

    init: 프로젝트 세팅[한재서]
    - changes:
      1. Spring Boot 프로젝트 세팅
      2. Spring DevTools, Lombok, JPA, Redis, MongoDB, Spring Security, Web, mysql driver, test 의존성 추가

---
+++ src/test/java/boot/kakaotech/communitybe/CommunityBeApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.kakaotech.communitybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
