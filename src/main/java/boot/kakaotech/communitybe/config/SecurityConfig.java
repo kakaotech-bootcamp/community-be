5daa7f080e98dc4e17d14008669a49e0d6cfd07e
JaeSeoHan <hanahyun1@gmail.com>
2025-10-02 15:27:30 +0900

    chore: SecurityConfig 세팅[한재서] - summary:   SecurityConfig 세팅 완료
    - changes:
      1. cors configuration source Bean으로 등록
        1-1. allow origins에 넣은 frontendUrl @Value로 설정
        1-2. allowedMethods 이 api에서 사용되는 GET, POST, PATCH, DELETE로 설정
      2. securityFilterChain 설정
        2-1. csrf 비활성화
        2-2. authorizeHttpRequests 설정(추후 url 추가 및 세분화 예정)
        2-3. formLogin 설정(추후 LoginSuccessHandler, LoginFailureHandler 추가 예정)
        2-4. logout 설정(추후 LogoutHandler 추가 예정)
        2-5. session 관리정책 STATELESS로 설정

---
+++ src/main/java/boot/kakaotech/communitybe/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package boot.kakaotech.communitybe.config;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityConfig {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(frontendUrl));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.info("[SecurityConfig] 보안 필터 구성 시작");
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> {
+                    requests
+                                    .requestMatchers(
+                                            "/"
+                                    ).permitAll()
+                                    .anyRequest().authenticated();
+                    log.info("[SecurityConfig] URL 인가 구성 완료");
+                })
+                .formLogin(form -> {
+                    form
+                            .loginProcessingUrl("/api/auth/signin")
+                    // TODO
+//                            .successHandler()
+//                            .failureHandler();
+                    ;
+                })
+                .logout(logout -> {
+                    logout
+                            .logoutUrl("/api/auth/logout")
+                            .addLogoutHandler((request, response, authentication) -> {
+                                response.setStatus(HttpServletResponse.SC_OK);
+                            })
+                            .permitAll();
+                })
+                .sessionManagement(session -> {
+                    log.info("[SecurityConfig] 세션 관리 정책: STATELESS");
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                });
+
+        return http.build();
+    }
+
+}
--- src/main/java/boot/kakaotech/communitybe/user/repository/UserRepository.java
+++ src/main/java/boot/kakaotech/communitybe/user/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package boot.kakaotech.communitybe.user.repository;
 
-public interface UserRepository {
+import boot.kakaotech.communitybe.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByEmail(String email);
+
 }
